4c4b48bf8f1ebc66b79e0f4fd4f9d229847235bc
anhhuy <phamanhhuy22@gmail.com>
2023-12-25 14:40:07 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ const wordRouter = require("./word");
 
 function route(app) {
 	app.use("/api/word", wordRouter);
+	app.get("/api/", function (req, res) {
+		res.send("Hello");
+	});
+	app.get("/favicon.ico", function (req, res) {
+		res.status(204);
+		res.end();
+	});
 }
 
 module.exports = route;
